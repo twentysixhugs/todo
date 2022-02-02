1492f72a45856300d70fa52f4ac5ee964814e881
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-02 17:42:11 +0300

    build adding projects and tasks

--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,80 @@ const DateFormatter = (function() {
     return {format};
 })();
 
-export {DateFormatter};
+//json should have a unique index for tasks, incremented every time a new task is created
+
+const storageUtils = (function () {
+    // const _taskIndex = get last index
+
+    const storage = {
+        "projectsData": JSON.parse(localStorage.getItem("projectsData")) || {},
+        "Today": JSON.parse(localStorage.getItem("Today")) || []
+    }
+
+
+    function init() {
+        const defaultInbox = {
+            "name": "Inbox",
+            "tasks": [
+
+            ]
+        }
+
+        if (storage.projectsData 
+            && Object.keys(storage.projectsData).length === 0 
+            && Object.getPrototypeOf(storage.projectsData) === Object.prototype) {
+
+            storage.projectsData["Inbox"] = defaultInbox;
+            _updateLocalStorage("projectsData")
+        }
+    }
+
+    function _updateLocalStorage(key) {
+        localStorage.removeItem(key);
+        localStorage.setItem(key, JSON.stringify(storage[key]));
+    }
+
+
+    function addProject(name) {
+        class Project {
+            constructor(name) {
+                this["name"] = name;
+                this["tasks"] = [];
+            }
+        }
+
+        const project = new Project(name);
+        storage.projectsData[name] = project;
+
+        _updateLocalStorage("projectsData");
+    }
+
+    function addTask(title, description, date, priority, project) {
+        class Task {
+            constructor(title, description, date, priority, project) {
+                this["title"] = title;
+                this["description"] = description;
+                this["date"] = date;
+                this["priority"] = priority;
+                this["project"] = project;
+            }
+        }
+
+        const task = new Task(title, description, date, priority, project);
+        storage.projectsData[project]["tasks"].push(task);
+
+        _updateLocalStorage("projectsData");
+    }
+
+    function getTask() {
+
+    }
+
+    function getProject() {
+
+    }
+
+    return {init, addProject, addTask};
+})();
+
+export {DateFormatter, storageUtils};

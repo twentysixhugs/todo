3c3063e9e74f7c2fc460940b14c5b82d72993ef8
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-04 12:25:37 +0300

    *add initial page generation *initialize ui for testing

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ storageUtils.init();
 // storageUtils.addTask("Test", "Test Description", "2023-02-02", "1", "Project 1");
 // storageUtils.addTask("000Test111", "Test Description", "2022-02-03", "0", "Project 1");
 
+
+//INIT!!!
 const projectsData = JSON.parse(localStorage.getItem("projectsData"));
 UILoader.Sidebar.addProject("Project 1", 26);
 UILoader.Sidebar.addEventsToProjects(projectsData);

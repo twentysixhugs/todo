83e6a3b63a28734e32fda0cf471c5a9576de7f7b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-02-05 15:36:11 +0300

    call init functions

--- src/index.js
+++ src/index.js
@@ -2,28 +2,4 @@ import UILoader from "./ui-loader"
 import {storageUtils} from "./utils"
 
 storageUtils.init();
-// storageUtils.addTask("!!!Test", "Test Description", "2022-02-03", "0", "Inbox");
-// storageUtils.addTask("Test", "Test Description", "2023-02-02", "1", "Inbox");
-// storageUtils.addTask("Test", "Test Description", "2023-02-02", "2", "Inbox");
-// storageUtils.addTask("!!!Test", "Test Description", "2022-02-03", "3", "Inbox");
-// storageUtils.addTask("Test", "Test Description", "2024-02-02", "1", "Inbox");
-
-// storageUtils.addProject("Project 1");
-
-// storageUtils.addTask("000Test111", "Test Description", "2022-02-03", "0", "Project 1");
-// storageUtils.addTask("Test", "Test Description", "2023-02-02", "0", "Project 1");
-// storageUtils.addTask("Test", "Test Description", "2023-02-02", "1", "Project 1");
-// storageUtils.addTask("000Test111", "Test Description", "2022-02-03", "0", "Project 1");
-
-
-//INIT!!!
-const projectsData = JSON.parse(localStorage.getItem("projectsData"));
-UILoader.Sidebar.addProject("Project 1", 26);
-UILoader.Sidebar.addEventsToProjects(projectsData);
-
-
-UILoader.Sidebar.addEventToToday(storageUtils.getTodayTasks());
-
-UILoader.ProjectWindow.initWindow("Today", storageUtils.getTodayTasks());
-UILoader.Header.init();
-//
+UILoader.load();
